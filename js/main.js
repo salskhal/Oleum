cd4e4f9bb1bd132ce23c14ff8537bf5b79fd710d
Khalid <olaniyisal@gmail.com>
2020-06-05 09:24:49 +0100

    Additional changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+    function myFunction() {
+        var x = document.getElementById("nav-content");
+        if (x.style.display === "flex") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "flex";
+        }
+    }
+
+
+   
+
